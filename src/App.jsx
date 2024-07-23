b6c6186b5ca725aa7a77cd6b398aeb00c1bfcbad
FarzanehDabiri71 <farzanehdabiri1371@yahoo.com>
2024-07-23 23:28:27 +0330

    import image

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// Importing the CSS file for styling the components
-import './App.css'
+import reactImg from "./assets/react-core-concepts.png";
 // Array of descriptions related to React
-const reactDescriptions = ['Fundamental', 'Cricial', 'Core'];
-// Function to generate a random integer between 0 and max 
-function genRandomInt (max) {
-  return Math.floor(Math.random() * (max + 1))
+const reactDescriptions = ["Fundamental", "Cricial", "Core"];
+// Function to generate a random integer between 0 and max
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
 }
+
 // Header component definition
-function Header(){
+function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="styleized atom" />
+      <img src={reactImg} alt="styleized atom" />
+      {/* <img src="src/assets/react-core-concepts.png" alt="styleized atom" /> */}
       <h1>React Essentials</h1>
       <p>
-         { reactDescriptions[genRandomInt(2)] } React concepts you will need for almost any app you are going to build.
+        {/* { reactDescriptions[genRandomInt(2)] }  */}
+        {description}
+        React concepts you will need for almost any app you are going to build.
       </p>
     </header>
-  )
+  );
 }
+
+
 // Main App component definition
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-         <Header/> 
+        <Header />
         <main>
           <h2>Time to get started!</h2>
         </main>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
