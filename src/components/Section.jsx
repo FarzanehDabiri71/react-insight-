05973e73e22873d0d4f778efc3f1e636e61fc982
FarzanehDabiri71 <farzanehdabiri1371@yahoo.com>
2024-08-01 10:28:34 +0330

    add comment

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,7 +2,8 @@
 export default function Section({title , children ,...props}){
     return(
            // Render a <section> HTML element and spread any additional props onto it
-    <section {...props}>
+           // ...props => id , className , ...
+    <section {...props}>   
             <h2>{title}</h2>
             {children}
         </section>
