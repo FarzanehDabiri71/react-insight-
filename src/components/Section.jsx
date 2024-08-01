294a0250fe61268a04a3073674356d42b851e49d
FarzanehDabiri71 <farzanehdabiri1371@yahoo.com>
2024-08-01 10:22:52 +0330

    Fix line ending issues

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,7 +1,8 @@
 // A flexible wrapper component for rendering a section of content.
 export default function Section({title , children ,...props}){
     return(
-        <section {...props}>
+           // Render a <section> HTML element and spread any additional props onto it
+    <section {...props}>
             <h2>{title}</h2>
             {children}
         </section>
